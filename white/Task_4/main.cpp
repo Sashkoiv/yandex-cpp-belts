b84e053e79008fdb9927a30bc69dedfb816b8414
Oleksandr Ivanchuk <sashkoiv@gmail.com>
2021-04-18 15:34:14 +0300

    White belt Task_4
    Correct CMakeLists names.

--- white/Task_4/main.cpp
+++ white/Task_4/main.cpp
@@ -7,4 +7,22 @@
  * 25 27 -> 1
  * 12 16 -> 4
  * 13 13 -> 13
- * /
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    for (int i = a; i > 0; i--) {
+        if (a % i == 0 && b % i == 0) {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
