0786892c451758192cb7c7a99657c154345fc466
Oleksandr Ivanchuk <sashkoiv@gmail.com>
2021-04-20 19:49:55 +0300

    White belt Task_8

--- white/Task_8/main.cpp
+++ white/Task_8/main.cpp
@@ -1,5 +1,35 @@
-/*
-Задание(перемещение строк):
-Напишите функцию MoveStrings, которая принимает два вектора строк, source и destination, и дописывает все строки из первого вектора в конец второго. После выполнения функции вектор source должен оказаться пустым.
-Моё решение:
-*/
+/*Задание(перемещение строк):
+ *
+ *Напишите функцию MoveStrings, которая принимает два вектора строк, source и
+ *destination, и дописывает все строки из первого вектора в конец второго.
+ *После выполнения функции вектор source должен оказаться пустым.
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void MoveStrings(vector<string>& source, vector<string>& destination) {
+    while (!source.empty())
+    {
+        destination.push_back(source.back());
+        source.pop_back();
+    }
+}
+
+int main() {
+    vector<string> src{"one", "two"};
+    vector<string> dest{"three", "four"};
+
+    cout << src.size() << endl;
+    cout << dest.size() << endl;
+
+    MoveStrings(src, dest);
+
+    cout << src.size() << endl;
+    cout << dest.size() << endl;
+
+    return 0;
+}
